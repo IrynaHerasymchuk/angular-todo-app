c93f770ffa66a4bf34000d0285dd99c2556c2de4
Iryna Herasymchuk <iherasymchuk@taskmaverick.com>
2023-06-21 18:34:21 +0300

    Implemented initial todo counting logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Todo {
+  id: number,
+  title: string,
+  completed: boolean,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  todos: Todo[] = [
+    { id: 1, title: 'HTML + CSS', completed: true },
+    { id: 2, title: 'JS', completed: false },
+    { id: 3, title: 'React', completed: false },
+    { id: 4, title: 'Angular', completed: false },
+  ];
+
   title = 'angular-todo-app';
+  isEditing = false;
+  isSaving = false;
+  activeTodos: Todo[] = [];
+
+  ngOnInit() {
+    this.countActiveTodos();
+  }
+
+  countActiveTodos() {
+    this.activeTodos = this.todos.filter(todo => !todo.completed);
+  }
 }
